cc67d522d549de7d4346362dfe6a88634848c9bb
rsbqr <rsbqr.coder@gmail.com>
2020-11-12 04:19:55 +0700

    tes hello

---
+++ a.hello.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HelloApp = () => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Text>Assalamu'alaikum</Text>
+        </View>
+    )
+}
+
+export default HelloApp;
